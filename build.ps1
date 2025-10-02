c6a70c5ee9eb49695e87da99b772282be46122db
Søren Hegelund Maagaard <maagaard100@gmail.com>
2025-10-02 10:10:44 +0200

    changes

--- build.ps1
+++ build.ps1
@@ -1,27 +1,47 @@
 #chocolatey packages to install
-$WorkPackages = "vscode","git","7zip","slack","teamviewer","pwsh","firacode-ttf","oh-my-posh"
-$PersonalPackages = "spotify","nordvpn"
+#$WorkPackages = "vscode","git","7zip","slack","teamviewer","pwsh","firacode-ttf","oh-my-posh", "nerd-fonts-hack"
+#$PersonalPackages = "spotify","nordvpn"
+
+winget install JanDeDobbeleer.OhMyPosh --source winget --scope user --force
+
+oh-my-posh font install firacode --user
+oh-my-posh font install meslo --user
+
+$currentDrive = $PSScriptRoot | Split-Path -Qualifier
+
 
 Import-Module ".\scripts\DotFilesSetup" -Force
 
 #install tools
-Install-ChocoPackages @($WorkPackages + $PersonalPackages)
+#Install-ChocoPackages @($WorkPackages + $PersonalPackages)
 
 #vscode
 Set-DotFile ".\vscode\settings.json" "$($env:APPDATA)\Code\User"
 
-#powershell
+# #powershell
 Set-SymLinkProcessProfile "powershell" ".\powershell\profile.ps1"
 Set-SymLinkProcessProfile "pwsh" ".\powershell\profile.ps1"
 
 #git
+#replace drive letter with current drive in gitconfig
+(Get-Content ".\git\gitconfig" -Raw) -replace "gitdir/i:[A-Z]:", "gitdir/i:$currentDrive" | Set-Content ".\git\gitconfig"
+
 Set-DotFile ".\git\gitconfig" "~" -addDot
 Set-DotFile ".\git\gitconfig.personal" "~" -addDot
 Set-DotFile ".\git\gitconfig.work" "~" -addDot
 Set-DotFile ".\git\gitignore.global" "~" -addDot
 
 Set-DotFile ".\ssh\config" "~\.ssh"
-Set-DotFile ".\terminal\profiles.json" "$($env:LOCALAPPDATA)\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+Set-DotFile ".\terminal\settings.json" "$($env:LOCALAPPDATA)\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 
-#wsl2
+# #wsl2
 Set-Dotfile ".\wsl\wslconfig" "~" -addDot
+
+# set nuget cache if dev drive is used
+if ($currentDrive -ne "C:") {
+  $newNugetDir = "$currentDrive\.nuget\packages"
+  $newHttpCacheDir = "$currentDrive\.nuget\v3-cache"
+  New-Item -Path $newNugetDir -ItemType Directory -Force -ErrorAction Ignore
+  # Copy-Item "$env:USERPROFILE\.nuget\packages\*.*" -Recurse "$currentDrive\.nuget\packages"
+  [System.Environment]::SetEnvironmentVariable('NUGET_PACKAGES',$newNugetDir,'User')
+}
--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,117 +1,12 @@
 Set-PSReadlineOption -BellStyle None
 
-# Modules should be installed on User scope
-# if Modules are not installed on User scope please run as admin:
-# Uninstall-Module -Name Module
-function Get-EnsureModule {
-  param(
-    [parameter(Mandatory, ValueFromPipeline)]
-    [string[]] $modulesNames
-  )
-  Process {
-    foreach ($moduleName in $modulesNames) {
-      if (!(Get-Module -Name $moduleName)) {
-        try {
-          Import-Module $moduleName -ErrorAction Stop
-        } catch {
-          Install-Module $moduleName -Scope CurrentUser -Force -AllowClobber
-          Import-Module $moduleName
-        }
-      }
-    }
-  }
-}
 
-function Install-Modules {
-  param(
-    [parameter(Mandatory, ValueFromPipeline)]
-    [string[]] $modulesNames
-  )
-  Begin {
-    Write-Host "Installing Modules..."
-  }
-  Process {
-    $installedModules = Get-InstalledModule
-    $checkRepo = $true
-    if ($checkRepo) {
-      Update-Repo
-      $checkRepo = $false
-    }
-    foreach ($moduleName in $modulesNames) {
-      if (!(Get-Module -Name $moduleName)) {
-        if ($installedModules.Name -notcontains $moduleName) {
-          Write-Host "Installing $moduleName"
-          Install-Module $moduleName -Scope CurrentUser -Force -AllowClobber
-        }
-      }
-    }
-  }
-  End {
-    Write-Host "Modules Installed"
-  }
-}
-
-function Update-Modules {
-  Update-Repo
-  $installedModules = Get-InstalledModule
-  foreach ($module in $installedModules) {
-    Try {
-      Write-Host "Checking $($module.name)"
-      $online = Find-Module $module.name
-    } Catch {
-      Write-Warning "Module $($module.name) was not found in the PSGallery"
-    }
-    if ($online.version -gt $module.version) {
-      Write-Host "Updating $($module.name) module"
-      Update-Module -Name $module.name
-    }
-  }
-}
-
-function Update-Repo {
-  # Ensure package mangers are installed
-  $packageProviders = PackageManagement\Get-PackageProvider -ListAvailable
-  $checkPowerShellGet = $packageProviders | Where-Object name -eq "PowerShellGet"
-  $checkNuget = $packageProviders | Where-Object name -eq "NuGet"
-  $checkPSGallery = Get-PSRepository PSGallery
-  if (!$checkPSGallery -or $checkPSGallery.InstallationPolicy -ne 'Trusted') {
-    Set-PSRepository PSGallery -InstallationPolicy trusted -SourceLocation "https://www.powershellgallery.com/api/v2"
-  }
-  if (!$checkPowerShellGet) {
-    PackageManagement\Get-PackageProvider -Name PowerShellGet -Force
-  }
-  if (!$checkNuget) {
-    PackageManagement\Get-PackageProvider -Name NuGet -Force
-  }
-}
+oh-my-posh init pwsh | Invoke-Expression
 
 
-$modules = (
-  "Get-ChildItemColor",
-  "DockerCompletion",
-  "posh-git",
-  "PSReadLine",
-  "cd-extras"
-)
 
-$_PSVersion = $PSVersionTable.PSVersion.Major
-$_File = "$PSScriptRoot/installed/$_PSVersion.test"
-if (!(Test-Path $_File)) {
-  Update-Repo
-  $modules | Install-Modules
-  New-Item -Path $_File -ItemType File -Force | Out-Null
-}
-
-$modules | Get-EnsureModule
-
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-
-# setup oh-my-posh
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\paradox.omp.json" | Invoke-Expression
-
-$developmentWorkspace = @("C:\git\personal", "C:\git\work")
-# setup cd extras
-$cde.CD_PATH = @($developmentWorkspace)
-
-# clear variables
-Remove-Variable _PSVersion, _File, modules
+# "Get-ChildItemColor",
+#   "DockerCompletion",
+#   "posh-git",
+#   "PSReadLine",
+#   "cd-extras"
